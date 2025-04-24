94fdf01a131f3860f36a6714b544575eb3a124d4
wiseaidev <oss@wiseai.dev>
2025-04-24 15:32:52 +0300

    chore: update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/opensass/cli/refs/heads/main/assets/logo.webp",
+    html_favicon_url = "https://github.com/opensass/cli/blob/main/assets/favicon.png"
+)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc = include_str!("../README.md")]
+
 pub mod cli;
 pub mod cmds;
 pub mod utils;
